41b44e4966c5d39ca22ff6c3f1be2577aa8ae7b5
Roshan Patil <84975574+roshan2610@users.noreply.github.com>
2024-01-05 10:33:52 +0530

    Delete ToDo App directory

--- ToDo App/ToDo-App/src/main/java/com/example/ToDoApp/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.ToDoApp.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-	public ResourceNotFoundException(String msg)
-	{
-		super(msg);
-	}
-}
